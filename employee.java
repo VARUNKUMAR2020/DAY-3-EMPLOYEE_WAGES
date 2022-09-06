0613ad642fbf4e7a1a0cb6a8cc2810da1c33c0a3
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-06 06:10:20 +0530

    TO CHECK THE EMPLOYEE DETAILS

---
+++ employee.java
@@ -0,0 +1,13 @@
+package Employee_Details;
+
+/**
+ * Hello world!
+ *
+ */
+public class employee 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "WELCOME TO THE COMPANY" );
+    }
+}
