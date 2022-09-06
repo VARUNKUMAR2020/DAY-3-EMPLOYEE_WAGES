99fd8e03681bc9739adb5e4062eb10c275cc21d3
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-06 06:13:48 +0530

    Add: Attendence of the employee

--- employee.java
+++ employee.java
@@ -9,5 +9,11 @@ public class employee
     public static void main( String[] args )
     {
         System.out.println( "WELCOME TO THE COMPANY" );
+    	int fulltime = 1;
+    	double empcheck = Math.floor(Math.random() * 10)% 2;
+    	if (empcheck == fulltime)
+    		System.out.println("Employee is Present");
+    	else
+    		System.out.println("Employee is Abscent");
     }
 }
